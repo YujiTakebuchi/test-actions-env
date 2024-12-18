51b37fc407cf2718d1f6dbb5aa7bfc542bdc36b0
YujiTakebuchi <takeyou.swift@gmail.com>
2024-12-18 18:50:15 +0900

    環境変数読み出し追加

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
+<script lang="ts">
+  import { PUBLIC_TEST } from '$env/static/public';
+</script>
+<h1>Welcome to {PUBLIC_TEST}</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
